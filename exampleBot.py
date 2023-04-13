4983fb45e23e3627cf10854590592092f9a9b23d
Далер Хайраков <90933193+WayDBae@users.noreply.github.com>
2023-04-13 16:11:58 +0500

    Create exampleBot.py

---
+++ exampleBot.py
@@ -0,0 +1,117 @@
+When your create new file, rename it to: myTexts.py
+To access to Russian version. Create new file, rename it to: myTextsRu.py
+After all you need two files: myTexts.py and myTextsRu.py 
+
+# English version = myTexts.py
+# Business
+text2_0 = '📂Category: 💲 Business:\n'
+text2_1 = '💠 “It’s not about ideas. It’s about making ideas happen.” - Scott Belsky\n'
+text2_2 = '💠 “If you can’t feed a team with two pizzas, it’s too large.” - Jeff Bezos\n'
+text2_3 = '💠 “If people like you, they’ll listen to you, but if they trust you, they’ll do business with you.” - Zig Ziglar\n'
+text2_4 = '💠 “Make every detail perfect and limit the number of details to perfect.” - Jack Dorsey\n'
+text2_5 = '💠 “The last 10% it takes to launch something takes as much energy as the first 90%.” - Rob Kalin\n'
+text2_6 = '💠 “Best startups generally come from somebody needing to scratch an itch.” - Michael Arrington\n'
+text2_7 = '💠 “Some people dream of success, while other people get up every morning and make it happen.” - Wayne Huizenga\n'
+text2_8 = '💠 “Ideas are easy. Implementation is hard.” - Guy Kawasaki\n'
+text2_9 = '💠 “Every day that we spent not improving our products was a wasted day.” - Joel Spolsky\n'
+text2_10 = '💠 Fortunes are built during the down market and collected in the upmarket.” - Jason Calacanis\n'
+text2 = text2_0+text2_1+text2_2+text2_3+text2_4+text2_5+text2_6+text2_7+text2_8+text2_9+text2_10
+
+# Love
+text3_0 = '📂Category: 💘 Love:\n'
+text3_1 = '💠 “Life is the flower for which love is the honey.” - Victor Hugo\n'
+text3_2 = '💠 “Age does not protect you from love, but love to some extent protects you from age.” - Jeanne Moreau\n'
+text3_3 = '💠 “We accept the love we think we deserve.” - Stephen Chbosky\n'
+text3_4 = '💠 “Better to have loved and lost, than to have never loved at all.” - St. Augustine\n'
+text3_5 = '💠 “There is never a time or place for true love. It happens accidentally, in a heartbeat, in a single flashing, throbbing moment.” - Sarah Dessen\n'
+text3_6 = '💠 “Love is that condition in which the happiness of another person is essential to your own.” - Robert A. Heinlein\n'
+text3_7 = '💠 “Love does not begin and end the way we seem to think it does. Love is a battle, love is a war; love is a growing up.” - James Baldwin\n'
+text3_8 = '💠 “In the end we discover that to love and let go can be the same thing.” - Jack Kornfield\n'
+text3_9 = '💠 “Pleasure of love lasts but a moment. Pain of love lasts a lifetime.” - Bette Davis\n'
+text3_10 = '💠 “You know you’re in love when you can’t fall asleep because reality is finally better than your dreams.” - Dr. Seus\n'
+text3 = text3_0+text3_1+text3_2+text3_3+text3_4+text3_5+text3_6+text3_7+text3_8+text3_9+text3_10
+
+# Friendship
+text4_0 = '📂Category: 💯 Friendship:\n'
+text4_1 = '💠 “Don’t make friends who are comfortable to be with. Make friends who will force you to lever yourself up.” - Thomas J. Watson\n'
+text4_2 = '💠 “They may forget what you said, but they will never forget how you made them feel.” - Carl W. Buechner\n'
+text4_3 = '💠 “A real friend is one who walks in when the rest of the world walks out.” - Walter Winchell\n'
+text4_4 = '💠 “A friend is one who overlooks your broken fence and admires the flowers in your garden.” - Unknown\n'
+text4_5 = '💠 “A friend who understands your tears is much more valuable than a lot of friends who only know your smile.” - Unknown\n'
+text4_6 = '💠 “Time doesn’t take away from friendship, nor does separation.” - Tennessee Williams\n'
+text4_7 = '💠 “There’s not a word yet for old friends who’ve just met.” - Jim Henson\n'
+text4_8 = '💠 “A single rose can be my garden…a single friend, my world.” - Leo Buscaglia\n'
+text4_9 = '💠 “Friends are those rare people who ask how we are and then wait to hear the answer.” - Ed Cunningham\n'
+text4_10 = '💠 “Truly great friends are hard to find, difficult to leave, and impossible to forget.” — G. Randolfs\n'
+text4 = text4_0+text4_1+text4_2+text4_3+text4_4+text4_5+text4_6+text4_7+text4_8+text4_9+text4_10
+
+# Motivation
+text5_0 = '📂Category: 🔋 Motivation:\n'
+text5_1 = '💠 “Learn as if you will live forever, live like you will die tomorrow.” - Mahatma Gandhi\n'
+text5_2 = '💠 “When you change your thoughts, remember to also change your world.” - Norman Vincent Peale\n'
+text5_3 = '💠 “Success is not final; failure is not fatal: it is the courage to continue that counts.” - Winston Churchill\n'
+text5_4 = '💠 "It is better to fail in originality than to succeed in imitation." - Herman Melville\n'
+text5_5 = '💠 “I never dreamed about success. I worked for it.” - Estée Lauder\n'
+text5_6 = '💠 “Success is getting what you want, happiness is wanting what you get.” - W. P. Kinsella\n'
+text5_7 = '💠 “Either you run the day or the day runs you.” - Jim Rohn\n'
+text5_8 = '💠 “Setting goals is the first step in turning the invisible into the visible.” - Tony Robbins\n'
+text5_9 = '💠 “Experience is a hard teacher because she gives the test first, the lesson afterwards.” - Vernon Sanders Law\n'
+text5_10 = '💠 “You learn more from failure than from success. Don’t let it stop you. Failure builds character.” - Unknown\n'
+text5 = text5_0+text5_1+text5_2+text5_3+text5_4+text5_5+text5_6+text5_7+text5_8+text5_9+text5_10
+
+# Русская версия = myTextsRu.py
+# Бизнес
+text2_0 = '◈Категория: 💲 Бизнес\n'
+text2_1 = '💠 «Дело не в идеях. Речь идет о воплощении идей». - Скотт Бельски\n'
+text2_2 = '💠 «Если вы не можете накормить команду двумя пиццами, она слишком велика». - Джефф Безос\n'
+text2_3 = '💠 «Если вы людям нравитесь, они будут слушать вас, но если они доверяют вам, они будут иметь с вами дело». - Зиг Зиглар\n'
+text2_4 = '💠 «Сделайте каждую деталь идеальной и ограничьте количество деталей до совершенства». - Джек Дорси\n'
+text2_5 = '💠 «Последние 10%, необходимые для запуска чего-либо, требуют столько же энергии, сколько и первые 90%». - Роб Калин\n'
+text2_6 = '💠 «Лучшие стартапы обычно исходят от тех, кому нужно почесать зуд». - Майкл Аррингтон\n'
+text2_7 = '💠 «Некоторые люди мечтают об успехе, а другие встают каждое утро и добиваются успеха». - Уэйн Хьюзенга\n'
+text2_8 = '💠 «Идеи даются легко. Реализация сложная». - Гай Кавасаки\n'
+text2_9 = '💠 «Каждый день, который мы потратили на то, чтобы не улучшать наши продукты, был потрачен впустую». - Джоэл Спольски\n'
+text2_10 = '💠 «Состояния строятся во время спада рынка и накапливаются на рынке выше». - Джейсон Калаканис\n'
+text2 = text2_0+text2_1+text2_2+text2_3+text2_4+text2_5+text2_6+text2_7+text2_8+text2_9+text2_10
+
+# Любовь
+text3_0 = '◈Категория: 💘 Любовь\n'
+text3_1 = '💠 «Жизнь — это цветок, для которого любовь — мед». - Виктор Гюго\n'
+text3_2 = '💠 «Возраст не защищает от любви, но любовь в какой-то степени защищает от старости». - Жанна Моро\n'
+text3_3 = '💠 «Мы принимаем ту любовь, которую, как мы думаем, мы заслуживаем.» - Стивен Чбоски\n'
+text3_4 = '💠 «Лучше любить и потерять, чем никогда не любить». - Святой Августин\n'
+text3_5 = '💠 «Для настоящей любви никогда не бывает ни времени, ни места. Это происходит случайно, в одно мгновение, в один миг, пульсирующий миг». - Сара Дессен\n'
+text3_6 = '💠 «Любовь — это такое состояние, при котором счастье другого человека необходимо для твоего собственного». - Роберт А. Хайнлайн\n'
+text3_7 = '💠 «Любовь не начинается и не заканчивается так, как нам кажется. Любовь — это битва, любовь — это война; любовь — это взросление». - Джеймс Болдуин\n'
+text3_8 = '💠 «В конце концов мы обнаруживаем, что любить и отпускать может быть одно и то же». - Джек Корнфилд\n'
+text3_9 = '💠 «Удовольствие от любви длится лишь мгновение. Боль любви длится всю жизнь». - Бетт Дэвис\n'
+text3_10 = '💠 «Вы понимаете, что влюблены, когда не можете заснуть, потому что реальность, наконец, лучше, чем ваши мечты». - Доктор Сеус\n'
+text3 = text3_0+text3_1+text3_2+text3_3+text3_4+text3_5+text3_6+text3_7+text3_8+text3_9+text3_10
+
+# Дружба
+text4_0 = '◈Категория: 💯 Дружба\n'
+text4_1 = '💠 «Не заводи друзей, с которыми комфортно. Заведи друзей, которые заставят тебя подняться». - Томас Дж. Уотсон\n'
+text4_2 = '💠 «Они могут забыть, что вы сказали, но они никогда не забудут, что вы заставили их чувствовать». - Карл В. Бюхнер\n'
+text4_3 = '💠 «Настоящий друг — это тот, кто входит, когда весь остальной мир уходит». - Уолтер Винчелл\n'
+text4_4 = '💠 «Друг — это тот, кто смотрит на твой сломанный забор и любуется цветами в твоем саду». - Неизвестный\n'
+text4_5 = '💠 «Друг, который понимает твои слезы, гораздо ценнее многих друзей, которые знают только твою улыбку». - Неизвестный\n'
+text4_6 = '💠 «Время не отнимает у дружбы, как и разлука». - Теннесси Уильямс\n'
+text4_7 = '💠 «Еще ни слова для старых друзей, которые только что встретились». - Джим Хенсон\n'
+text4_8 = '💠 «Одна роза может быть моим садом… одинокий друг — мой мир». - Лео Бускалья\n'
+text4_9 = '💠 «Друзья — это те редкие люди, которые спрашивают, как у нас дела, и ждут ответа». - Эд Каннингем\n'
+text4_10 = '💠 «По-настоящему хороших друзей трудно найти, трудно расстаться и невозможно забыть». - Г. Рэндольфс\n'
+text4 = text4_0+text4_1+text4_2+text4_3+text4_4+text4_5+text4_6+text4_7+text4_8+text4_9+text4_10
+
+# Мотивация
+text5_0 = '◈Категория: 🔋 Мотивация\n'
+text5_1 = '💠 «Учись так, как будто будешь жить вечно, живи так, как будто умрешь завтра». - Махатма Ганди\n'
+text5_2 = '💠 «Когда вы меняете свои мысли, не забудьте также изменить свой мир». - Норман Винсент Пил\n'
+text5_3 = '💠 «Успех не окончательный; неудача не фатальна: важна смелость продолжать». - Уинстон Черчилль\n'
+text5_4 = '💠 «Лучше потерпеть неудачу в оригинальности, чем преуспеть в подражании». - Герман Мелвилл\n'
+text5_5 = '💠 «Я никогда не мечтал об успехе. Я работал для этого». - Эсти Лаудер\n'
+text5_6 = '💠 «Успех — это получать то, что хочешь, счастье — хотеть того, что получаешь». - В. П. Кинселла\n'
+text5_7 = '💠 «Либо ты управляешь днем, либо день управляет тобой». - Джим Рон\n'
+text5_8 = '💠 «Постановка целей — это первый шаг к превращению невидимого в видимое». - Тони Роббинс\n'
+text5_9 = '💠 «Опыт — суровый учитель, потому что сначала она дает тест, а потом — урок». - Вернон Сандерс\n'
+text5_10 = '💠 «Вы учитесь большему на неудачах, чем на успехах. Не позволяйте этому остановить вас. Неудача закаляет характер». - Неизвестный\n'
+text5 = text5_0+text5_1+text5_2+text5_3+text5_4+text5_5+text5_6+text5_7+text5_8+text5_9+text5_10
